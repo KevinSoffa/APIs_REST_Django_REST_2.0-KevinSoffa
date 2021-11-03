add622e6a6c5e29ff2103c58825ec70fde670e7c
Kevin Soffa <kevinsoffa@gmail.com>
2021-11-03 11:13:17 -0300

    terceiro deploy
    Corrigindo erros

--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -20,10 +21,17 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         if self.kwargs.get('curso_pk'):
-            return self.queryset.filter(curso_id=self.kwargs.get('curso_id'))
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
         return self.queryset.all()
 
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(),
+                                     curso_id=self.kwargs.get('curso_id'),
+                                     pk=self.kwargs.get('avaliacao.pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
